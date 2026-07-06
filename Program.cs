f35d15a44c3129dc9bcf7b392e8fca203fe8c73c
alexbarnu34 <163245277+alexbarnu34@users.noreply.github.com>
2026-07-06 11:39:40 +0300

    Numele meu2

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 Console.WriteLine("Darius");
 Console.WriteLine("Gabit");
 Console.WriteLine("Alex Barnu");
-Console.WriteLine("Alex Beu")
+Console.WriteLine("Alex Beu");
 Console.WriteLine("Coman David");
