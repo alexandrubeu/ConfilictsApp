8f71e5ea27850dee8d5ea94548517d8909d771de
Barna Tudor <53333717+barna-tudor@users.noreply.github.com>
2026-07-06 12:26:39 +0300

    Resolve conflict (#6)

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 Console.WriteLine("Darius");
+Console.WriteLine("Gabit");
 Console.WriteLine("Alex Barnu");
 Console.WriteLine("Alex Beu");
 Console.WriteLine("extra");
+Console.WriteLine("barna-tudor");
